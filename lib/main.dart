68fe4a5a4a202ca4ff871ecd4f813eabfc7417e6
lookmaniak <lookman.nh@gmail.com>
2021-08-02 00:32:55 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,36 @@
+import 'package:demogame/screens/GameSetting.dart';
 import 'package:demogame/screens/Home.dart';
-import 'package:demogame/screens/GatoGame.dart';
-import 'package:demogame/screens/ModoJuegoRapido.dart';
-import 'package:demogame/screens/ParesCardsGame.dart';
+import 'package:demogame/screens/SplashScreen.dart';
+import 'package:demogame/screens/UpiPresents.dart';
+import 'package:demogame/screens/Welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+final isMusicOn =
+    ValueNotifier<bool>(true);
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+    ]);
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Alferain Nurainia',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: "home",
+      initialRoute: "splash",
       routes: {
-        'home'          : (BuildContext context) => Home(),
-        'gatogame'      : (BuildContext context) => GatoGame(),
-        //'ahorcadogame'         : ( BuildContext context ) => AhorcadoGame(),
-        'cardsgame'     : ( BuildContext context ) => ParesCardsGame(),
-        'juegorapido'   : ( BuildContext context ) => ModoJuegoRapido(),
+        'splash'        : (BuildContext context) => SplashScreen(),
+        'upi_presents'  : (BuildContext context) => UpiPresents(),
+        'home'          : (BuildContext context) => Home(title: "alferain"),
+        'game_setting'  : (BuildContext context) => GameSetting(title: "setting"),
       },
     );
   }
